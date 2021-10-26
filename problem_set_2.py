79c11e9c9a3266f4e9555f30b3d9db4dc7b4e6f4
James Andrews <jbandrew14@gmail.com>
2021-10-26 18:41:48 -0400

    finished through problem 6

--- problem_set_2.py
+++ problem_set_2.py
@@ -5,7 +5,12 @@
 
 # If the item is not in the list, return the list unchanged.
 # If the item is in the list, remove all instances of it from the list.
-
+def remove(list_input, item):
+    list_copy = list_input
+    if item in list_copy:
+        while item in list_copy:
+            list_copy.remove(item)
+    return list_copy
 
 # ------------------------------------------------------------------------------
 # 2. Revisit your "remove" function. Make sure that it does not change the original
@@ -16,18 +21,29 @@
 # 3. Create a function called "sum_list" that takes a list of numbers and
 # returns the sum of those numbers. Write this function without using the
 # built-in `sum()` function.
-
+def sum_list(num_list):
+    total = 0
+    for number in num_list:
+        total += number
+    return total
 
 # ------------------------------------------------------------------------------
 # 4. Create a function called "average" that takes a list of numbers
 # and returns the average of those numbers.
+def average(num_list):
+    return sum_list(num_list) / len(num_list)
 
 
 # ------------------------------------------------------------------------------
 # 5. Create a function called "minimum" that takes a list of numbers and
 # returns the smallest number in that list. Write this function without using the
 # built-in `min()` function.
-
+def minimum(num_list):
+    minimum = num_list[0]
+    for number in num_list:
+        if number < minimum:
+            minimum = number
+    return minimum
 
 # ------------------------------------------------------------------------------
 # 6. There are many techniques to sort lists (or arrays) in programming. Many programming
